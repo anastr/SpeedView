45a836e2796182315e81e2e394ebe8b013d0741b
Anas Altair <anastr244@gmail.com>
2016-12-27 16:13:55 +0200

    add indicators to demo

--- app/src/main/java/com/github/anastr/speedview/MainActivity.java
+++ app/src/main/java/com/github/anastr/speedview/MainActivity.java
@@ -4,57 +4,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        String[] values = new String[] { "Speed View",
+                "Deluxe Speed View",
+                "Awesome Speedometer View",
+                "Ray Speedometer View",
+                "Pointer Speedometer",
+                "Tube Speedometer",
+                "Image Speedometer",
+                "Work With Indicator",
+                "Work With Note",
+                "Create Speedometer Programmatically" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(this);
     }
 
-    public void openSpeedViewActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, SpeedViewActivity.class);
-        startActivity(intent);
-    }
-
-    public void openDeluxeSpeedActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, DeluxeSpeedActivity.class);
-        startActivity(intent);
-    }
-
-    public void openAwesomeSpeedometerActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, AwesomeSpeedometerActivity.class);
-        startActivity(intent);
-    }
-
-    public void openRaySpeedometerActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, RayActivity.class);
-        startActivity(intent);
-    }
-
-    public void openPointerSpeedometerActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, PointerActivity.class);
-        startActivity(intent);
-    }
-
-    public void openTubeSpeedometerActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, TubeSpeedometerActivity.class);
-        startActivity(intent);
-    }
-
-    public void openImageSpeedometerActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, ImageSpeedometerActivity.class);
-        startActivity(intent);
-    }
-
-    public void openWorkWithNoteActivity(View view) {
-        Intent intent = new Intent (MainActivity.this, WorkWithNoteActivity.class);
-        startActivity(intent);
-    }
-
-    public void openCreateProgrammatically(View view) {
-        Intent intent = new Intent (MainActivity.this, CreateProgrammatically.class);
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Intent intent = new Intent ();
+        switch (position) {
+            case 0:
+                intent.setClass(MainActivity.this, SpeedViewActivity.class);
+                break;
+            case 1:
+                intent.setClass(MainActivity.this, DeluxeSpeedActivity.class);
+                break;
+            case 2:
+                intent.setClass(MainActivity.this, AwesomeSpeedometerActivity.class);
+                break;
+            case 3:
+                intent.setClass(MainActivity.this, RayActivity.class);
+                break;
+            case 4:
+                intent.setClass(MainActivity.this, PointerActivity.class);
+                break;
+            case 5:
+                intent.setClass(MainActivity.this, TubeSpeedometerActivity.class);
+                break;
+            case 6:
+                intent.setClass(MainActivity.this, ImageSpeedometerActivity.class);
+                break;
+            case 7:
+                intent.setClass(MainActivity.this, WorkWithIndicatorActivity.class);
+                break;
+            case 8:
+                intent.setClass(MainActivity.this, WorkWithNoteActivity.class);
+                break;
+            case 9:
+                intent.setClass(MainActivity.this, CreateProgrammatically.class);
+                break;
+        }
         startActivity(intent);
     }
 }
