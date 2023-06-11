fd42d5d064c798fa7fb85a06456de4847907d302
Anas Altair <anastr244@gmail.com>
2023-06-11 09:56:38 +0400

    Convert ImageSpeedometerActivity to kotlin.

--- app/src/main/java/com/github/anastr/speedviewapp/ImageSpeedometerActivity.kt
+++ app/src/main/java/com/github/anastr/speedviewapp/ImageSpeedometerActivity.kt
@@ -1,55 +1,40 @@
-package com.github.anastr.speedviewapp;
+package com.github.anastr.speedviewapp
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.github.anastr.speedviewlib.ImageSpeedometer
+import java.util.Locale
 
-import androidx.appcompat.app.AppCompatActivity;
+class ImageSpeedometerActivity : AppCompatActivity() {
 
-import com.github.anastr.speedviewlib.ImageSpeedometer;
+    private lateinit var imageSpeedometer: ImageSpeedometer
+    private lateinit var seekBar: SeekBar
+    private lateinit var ok: Button
+    private lateinit var textSpeed: TextView
 
-import java.util.Locale;
-
-public class ImageSpeedometerActivity extends AppCompatActivity {
-
-    ImageSpeedometer imageSpeedometer;
-    SeekBar seekBar;
-    Button ok;
-    TextView textSpeed;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_speedometer);
-        setTitle("Image Speedometer");
-
-        imageSpeedometer = findViewById(R.id.imageSpeedometer);
-        seekBar = findViewById(R.id.seekBar);
-        ok = findViewById(R.id.ok);
-        textSpeed = findViewById(R.id.textSpeed);
-
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageSpeedometer.speedTo(seekBar.getProgress());
-            }
-        });
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textSpeed.setText(String.format(Locale.getDefault(), "%d", progress));
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_speedometer)
+        title = "Image Speedometer"
+        imageSpeedometer = findViewById(R.id.imageSpeedometer)
+        seekBar = findViewById(R.id.seekBar)
+        ok = findViewById(R.id.ok)
+        textSpeed = findViewById(R.id.textSpeed)
+        ok.setOnClickListener {
+            imageSpeedometer.speedTo(
+                seekBar.progress.toFloat()
+            )
+        }
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                textSpeed.text = String.format(Locale.getDefault(), "%d", progress)
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
 }
