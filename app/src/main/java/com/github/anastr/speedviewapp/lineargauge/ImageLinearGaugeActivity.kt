d83f1497a30d99c1a3b9c74d5514cf5debed5286
Anas Altair <anastr244@gmail.com>
2023-06-11 07:36:52 +0400

    Convert ImageLinearGaugeActivity to kotlin.

--- app/src/main/java/com/github/anastr/speedviewapp/lineargauge/ImageLinearGaugeActivity.kt
+++ app/src/main/java/com/github/anastr/speedviewapp/lineargauge/ImageLinearGaugeActivity.kt
@@ -1,71 +1,50 @@
-package com.github.anastr.speedviewapp.lineargauge;
+package com.github.anastr.speedviewapp.lineargauge
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.github.anastr.speedviewapp.R
+import com.github.anastr.speedviewlib.ImageLinearGauge
+import com.github.anastr.speedviewlib.LinearGauge
+import java.util.Locale
 
-import androidx.appcompat.app.AppCompatActivity;
+class ImageLinearGaugeActivity : AppCompatActivity() {
 
-import com.github.anastr.speedviewapp.R;
-import com.github.anastr.speedviewlib.ImageLinearGauge;
-import com.github.anastr.speedviewlib.LinearGauge;
+    private lateinit var imageLinearGauge: ImageLinearGauge
+    private lateinit var seekBar: SeekBar
+    private lateinit var ok: Button
+    private lateinit var textSpeed: TextView
+    private lateinit var checkBoxOrientation: CheckBox
 
-import java.util.Locale;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_linear_gauge)
 
-public class ImageLinearGaugeActivity extends AppCompatActivity {
+        title = "Image Linear Gauge"
+        imageLinearGauge = findViewById(R.id.gauge)
+        seekBar = findViewById(R.id.seekBar)
+        ok = findViewById(R.id.ok)
+        textSpeed = findViewById(R.id.textSpeed)
+        checkBoxOrientation = findViewById(R.id.cb_orientation)
 
-    ImageLinearGauge imageLinearGauge;
-    SeekBar seekBar;
-    Button ok;
-    TextView textSpeed;
-    CheckBox checkBoxOrientation;
+        ok.setOnClickListener { imageLinearGauge.speedTo(seekBar.progress.toFloat()) }
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    textSpeed.text = String.format(Locale.getDefault(), "%d", progress)
+                }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_linear_gauge);
-        setTitle("Image Linear Gauge");
-
-        imageLinearGauge = (ImageLinearGauge) findViewById(R.id.gauge);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
-        ok = (Button) findViewById(R.id.ok);
-        textSpeed = (TextView) findViewById(R.id.textSpeed);
-        checkBoxOrientation = (CheckBox) findViewById(R.id.cb_orientation);
-
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageLinearGauge.speedTo(seekBar.getProgress());
+                override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
             }
-        });
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textSpeed.setText(String.format(Locale.getDefault(), "%d", progress));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-
-        checkBoxOrientation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b)
-                    imageLinearGauge.setOrientation(LinearGauge.Orientation.VERTICAL);
-                else
-                    imageLinearGauge.setOrientation(LinearGauge.Orientation.HORIZONTAL);
-            }
-        });
+        )
+        checkBoxOrientation.setOnCheckedChangeListener { _, b ->
+            if (b) imageLinearGauge.orientation =
+                LinearGauge.Orientation.VERTICAL else imageLinearGauge.orientation =
+                LinearGauge.Orientation.HORIZONTAL
+        }
     }
 }
